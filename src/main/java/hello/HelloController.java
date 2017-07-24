f597f3f92966351e8c68adb6872ddc97b70cafd6
Engen, Henning <henning@engen.online>
2017-07-25 00:29:34 +0200

    Adds api base path for mvc controllers

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("greeting")
     public Greeting greeting() {
         return new Greeting(1, "engen.online");
     }
